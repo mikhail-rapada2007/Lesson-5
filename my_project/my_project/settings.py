73f7410329801aaf6ecede37c707e7fbf2ddeb97
Mikhail Rapada <mprapada@student.hau.edu.ph>
2026-07-20 19:20:51 +0800

    yes

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -115,3 +116,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+STATICFILES_DIRS = {
+    BASE_DIR / "static_my_project",
+}
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static", "static_root")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media_root")
